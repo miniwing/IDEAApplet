40f329dcc74e7b636282b59266f7c06338176d6b
Harry <miniwing.hz@gmail.com>
2021-04-30 11:44:15 +0800

    no message

--- applet-framework/samurai-core/modules/IDEAAppletSingleton.h
+++ applet-framework/samurai-core/modules/IDEAAppletSingleton.h
@@ -42,12 +42,12 @@
 #undef  def_singleton
 #define def_singleton( __class )                                  \
         dynamic sharedInstance;                                   \
-        - (__class *)sharedInstance                               \
-        {                                                         \
+        - (__class *)sharedInstance {                             \
+                                                                  \
            return [__class sharedInstance];                       \
         }                                                         \
-        + (__class *)sharedInstance                               \
-        {                                                         \
+        + (__class *)sharedInstance {                             \
+                                                                  \
            static dispatch_once_t once;                           \
            static __strong id __singleton__ = nil;                \
            dispatch_once( &once, ^{ __singleton__ = [[__class alloc] init]; } ); \
